ea419291009524be639750dfa2131034f598a68b
Alina Cornea <alinacornea18@gmail.com>
2017-08-31 23:05:28 -0700

    adding

--- shared/header.php
+++ shared/header.php
@@ -8,14 +8,11 @@
   <meta charset="utf-8">
   <title>Camagru</title>
   <link rel="icon" href="http://localhost:8080/camagru/front/images/icon_star.png"/>
-  <link rel="stylesheet" href="http://localhost:8080/camagru/front/style/header.css">
+  <link rel="stylesheet" href="http://localhost:8080/camagru/front/style/header_style.css">
 </head>
-
-<body>
     <header>
       <h1>
-        <a title="Home"href="http://localhost:8080/camagru/index.php"> <img id="home" src="http://localhost:8080/camagru/front/images/home.png" alt="Home"></a>
-        <svg >
+        <svg  height="70px">
         <radialGradient id="gr-radial"
                     cx="100%" cy="100%" r="100%">
       <animate attributeName="r"
@@ -29,18 +26,21 @@
       <stop stop-color="rgba(250, 180, 150, 2)" offset="100%"/>
     </radialGradient>
     <text text-anchor="middle"
-          x="50%"
-          y="50%"
-          dy=".10em"
+          x="40%"
+          y="55%"
+          dy=".0001em"
           class="text"
           >
       Camagru -
     </text>
+    <div id="create"> <?php if ($_SESSION[login]) echo $_SESSION[login].","; ?>  create your world !!
   </svg>
+  <a title="Home"href="http://localhost:8080/camagru/index.php"> <img id="home" src="http://localhost:8080/camagru/front/images/home.png" alt="Home"></a>
         <a title="Admin" href="http://localhost:8080/camagru/admin/user/admin.php"><img id="admin" src="http://localhost:8080/camagru/front/images/admin.png" alt="Admin"></a>
         <a title="View" href="http://localhost:8080/camagru/front/view.php"><img id="view" src="http://localhost:8080/camagru/front/images/view.png" alt="View"></a>
         <a title="Logout" href="http://localhost:8080/camagru/admin/user/logout.php"><img id="logout" src="http://localhost:8080/camagru/front/images/out.png" alt="Logout"></a>
         <a title="Login" href="http://localhost:8080/camagru/admin/user/login.php"> <img id="login"src="http://localhost:8080/camagru/front/images/log.png" alt="Login"></a>
-        <div id="create"><?php if ($_SESSION[login]) echo $_SESSION[login].","; ?>  create your world !!</div>
       </h1>
+      </div>
     </header>
+<body>
